3d3f970aa8b12d1cfebbcac03d4f30e09de96afa
Daniel <olafimihandaniel92@gmail.com>
2022-10-13 16:59:33 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- entry point
+ *
+ *Return: zero if no error
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
