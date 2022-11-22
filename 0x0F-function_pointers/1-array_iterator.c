54603f93879ed1a04192ae59df03f353fd67f211
Keystrings <olafimihandaniel92@gmail.com>
2022-11-22 22:09:53 +0100

    functionspointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,4 +7,13 @@
  * @size: size of the array.
  * @action: pointer to the function.
  *
- *
+ * Return: no return.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
